2ec57834cd6e6be84e81f4f089f9596b386ae83f
qcyblm <qcyblm@outlook.com>
2020-11-14 21:39:19 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,21 @@ npm run docs:build
 # 进入生成的文件夹
 cd src/.vuepress/dist
 
+# deploy to github
+if [ -z "$GITHUB_TOKEN" ]; then
+  msg='deploy'
+  githubUrl=git@github.com:qcyblm/vuepress-theme-vpx.git
+else
+  msg='来自GitHub Action的自动部署'
+  githubUrl=https://qcyblm:${GITHUB_TOKEN}@github.com/qcyblm/vuepress-theme-vpx.git
+  git config --global user.name "qcyblm"
+  git config --global user.email "${ACCESS_EMAIL}"
+fi
+git init
+git add -A
+git commit -m "${msg}"
+git push -f $githubUrl master:gh-pages # 推送到github
+
 # Deploy to Gitee
 if [ -z "$GITEE_TOKEN" ]; then  # -z 字符串 长度为0则为true；$GITEE_TOKEN来自于GitHub仓库`Settings/Secrets`设置的私密环境变量
   msg='来自GitHub Action的自动部署'
@@ -22,6 +37,6 @@ git config user.name "qcyblm"
 git config user.email "qcyblm@163.com"
 git add -A
 git commit -m "${msg}"
-git push $giteeUrl master # 推送到Gitee
+git push -f $giteeUrl master # 推送到Gitee
 
 cd - 
