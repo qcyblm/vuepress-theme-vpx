6fb7bfbec38d9377ddace6287bd0c0b0fa83a9c8
qcyblm <qcyblm@outlook.com>
2020-11-14 23:35:33 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -25,14 +25,6 @@ git add -A
 git commit -m "${msg}"
 git push -f $githubUrl master:gh-pages # 推送到github
 
-
-cd - 
-# 确保脚本抛出遇到的错误
-set -e
-
-# 进入生成的文件夹
-cd src/.vuepress/dist
-
 # Deploy to Gitee
 if [ -z "$GITEE_TOKEN" ]; then  # -z 字符串 长度为0则为true；$GITEE_TOKEN来自于GitHub仓库`Settings/Secrets`设置的私密环境变量
   giteeUrl=git@gitee.com:qcyblm/vuepress-theme-vpx.git
