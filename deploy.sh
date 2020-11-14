1dd44427930e538b8487a935cf469fc6cc7f2d66
qcyblm <qcyblm@outlook.com>
2020-11-14 23:29:10 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -35,17 +35,12 @@ cd src/.vuepress/dist
 
 # Deploy to Gitee
 if [ -z "$GITEE_TOKEN" ]; then  # -z 字符串 长度为0则为true；$GITEE_TOKEN来自于GitHub仓库`Settings/Secrets`设置的私密环境变量
-  msg='来自GitHub Action的自动部署'
   giteeUrl=git@gitee.com:qcyblm/vuepress-theme-vpx.git
 else
-  msg='来自GitHub Action的自动部署'
   giteeUrl=https://qcyblm:${GITEE_TOKEN}@gitee.com/qcyblm/vuepress-theme-vpx.git # 使用GitHub CI
   git config --global user.name "qcyblm"
   git config --global user.email "qcyblm@163.com"
 fi
-git init
-git add -A
-git commit -m "${msg}"
 git push -f $giteeUrl master # 推送到Gitee
 
 cd - 
