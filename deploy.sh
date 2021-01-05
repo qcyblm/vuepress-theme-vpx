5ef1379a10e0c452a60df9e426da60cdbee81409
qcyblm <qcyblm@outlook.com>
2021-01-05 19:02:03 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -25,14 +25,4 @@ git add -A
 git commit -m "${msg}"
 git push -f $githubUrl master:gh-pages # 推送到github
 
-# Deploy to Gitee
-if [ -z "$GITEE_TOKEN" ]; then  # -z 字符串 长度为0则为true；$GITEE_TOKEN来自于GitHub仓库`Settings/Secrets`设置的私密环境变量
-  giteeUrl=git@gitee.com:qcyblm/vuepress-theme-vpx.git
-else
-  giteeUrl=https://qcyblm:${GITEE_TOKEN}@gitee.com/qcyblm/vuepress-theme-vpx.git # 使用GitHub CI
-  git config --global user.name "qcyblm"
-  git config --global user.email "${GITEE_EMAIL}"
-fi
-git push -f $giteeUrl master # 推送到Gitee
-
 cd - 
